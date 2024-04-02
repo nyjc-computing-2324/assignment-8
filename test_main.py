87e62c6cd9f3c5282ef8962d324f9c3ea8bec7bc
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-04-02 16:50:11 +0800

    fix detection of autograding module in test_main.py

--- test_main.py
+++ test_main.py
@@ -1,23 +1,41 @@
+from importlib import reload
 import sys
 import unittest
 
 import main
 
 
-# autograding submodule might not be successfully pulled on init
-# if unsuccessful, we have to pull it manually
 # limit number of retries
 MAX_RETRIES = 5
 retries = 0
 
-while "autograding" not in sys.modules:
+
+def autograding_successfully_imported():
+    """Check if autograding module is successfully imported.
+
+    If a submodule is imported (e.g. autograding.case), autograding may be imported
+    as a namespace module.
+    Namespace modules have __file__ attribute set tot None, so we can use that to check
+    if autograding was imported as a module and not just a namespace module.
+    """
+    return (
+        "autograding" in locals()
+        and locals().get("autograding").__file__
+    )
+
+# autograding submodule might not be successfully fetched on init
+# if unsuccessful, we have to fetch it manually
+while not autograding_successfully_imported():
     try:
         import autograding
+        reload(autograding)
         from autograding.case import FuncCall, InOut, RecursiveCall
-    except ImportError:
+    except (ImportError, ModuleNotFoundError):
         import subprocess
         subprocess.run(["git", "submodule", "update", "--init"])
         retries += 1
+    else:
+        break
     if retries >= MAX_RETRIES:
         sys.exit("[import autograding] Too many retries, exiting")
 
